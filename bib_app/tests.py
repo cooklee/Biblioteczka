50f5c371068e57109ee05567a77cf28343ad8da9
cooklee <sbbogus@gmail.com>
2022-02-06 11:28:21 +0100

    testy django

--- bib_app/tests.py
+++ bib_app/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import pytest
+from django.test import TestCase, Client as WebClient
 
 # Create your tests here.
+from django.urls import reverse
+
+
+def test_index(client):
+    url = reverse('index')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_authors(client):
+    url = reverse('authors')
+    response = client.get(url)
+    assert  response.status_code == 200
